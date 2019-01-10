21289dc3555dcc7c2e19123fa66caa07a7ffda98
Kamolnadda Dansuputra <kamolnaddadansuputra@Kamolnaddas-MacBook-Pro.local>
2019-01-10 18:42:13 +0700

    Updates

---
+++ precache-manifest.0d32f5773ef122c14a92e78d5aaad654.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5be9b9dae6c06f5954e716dc93e229fc",
+    "url": "/portfolio/static/media/bg.5be9b9da.png"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "dce705139438750607f4",
+    "url": "/portfolio/static/js/main.dce70513.chunk.js"
+  },
+  {
+    "revision": "5059622ad9a252b56640",
+    "url": "/portfolio/static/js/1.5059622a.chunk.js"
+  },
+  {
+    "revision": "dce705139438750607f4",
+    "url": "/portfolio/static/css/main.f77c3d3b.chunk.css"
+  },
+  {
+    "revision": "5059622ad9a252b56640",
+    "url": "/portfolio/static/css/1.aab8c428.chunk.css"
+  },
+  {
+    "revision": "adad727e3c1821ca876901087269908b",
+    "url": "/portfolio/index.html"
+  }
+];
